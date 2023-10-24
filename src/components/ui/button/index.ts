b0d90ab5ada8ac9a09883467a2825afeb1b0ec35
technorav3nn <Colin01460@outlook.com>
2023-10-24 19:09:16 -0400

    feat: fix fonts by using google fonts

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -1,6 +1,6 @@
 import { cva } from "class-variance-authority";
 
-export { default as Button } from "./Button.vue";
+export { default as Button } from "./button.vue";
 
 export const buttonVariants = cva(
 	"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
